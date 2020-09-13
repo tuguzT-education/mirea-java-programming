737902e48767fd768b60cc09e03488d0803fa00c
tuguzT <56771526+tuguzT@users.noreply.github.com>
2020-09-13 13:31:14 +0300

    Lab 5 was updated

--- src/rtu/lab5/Lab5.java
+++ src/rtu/lab5/Lab5.java
@@ -2,45 +2,51 @@ package rtu.lab5;
 
 import javax.swing.*;
 import java.awt.*;
+import javax.imageio.ImageIO;
+import java.io.File;
+import java.io.IOException;
 
 public class Lab5 extends JFrame {
     private final JLabel label;
     private final ImageIcon[] imageIcons;
 
-    Lab5() {
+    Lab5() throws IOException {
         super("AnimatedFrame");
         setSize(500, 500);
 
         imageIcons = new ImageIcon[] {
-                new ImageIcon(new ImageIcon("C:/Users/mi/Documents/IntelliJ Projects" +
-                        "/RTU Java/src/rtu/lab5/frames/cursed.png").getImage()
-                        .getScaledInstance(getWidth(), getHeight(), Image.SCALE_SMOOTH)),
-                new ImageIcon(new ImageIcon("C:/Users/mi/Documents/IntelliJ Projects" +
-                        "/RTU Java/src/rtu/lab5/frames/timur.jpg").getImage()
-                        .getScaledInstance(getWidth(), getHeight(), Image.SCALE_SMOOTH)),
+                new ImageIcon(ImageIO.read(new File("C:/Users/mi/Documents/IntelliJ Projects" +
+                        "/RTU Java/src/rtu/lab5/frames/cursed.png"))
+                        .getScaledInstance(getWidth(), getHeight(), Image.SCALE_AREA_AVERAGING)),
+                new ImageIcon(ImageIO.read(new File("C:/Users/mi/Documents/IntelliJ Projects" +
+                        "/RTU Java/src/rtu/lab5/frames/timur.jpg"))
+                        .getScaledInstance(getWidth(), getHeight(), Image.SCALE_AREA_AVERAGING))
         };
         label = new JLabel();
         label.setBounds(0, 0, getWidth(), getHeight());
+        add(label);
 
         setVisible(true);
         setResizable(false);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-
-        add(label);
     }
 
     public static void main(String[] args) {
-        var frame = new Lab5();
-        while (true) {
-            for (var imageIcon : frame.imageIcons) {
-                frame.label.setIcon(imageIcon);
-                try {
-                    Thread.sleep(300);
-                } catch (Exception e) {
-                    System.out.println("Couldn't wait for 300 milliseconds!");
-                    System.exit(0);
+        try {
+            var frame = new Lab5();
+            while (true) {
+                for (var imageIcon : frame.imageIcons) {
+                    frame.label.setIcon(imageIcon);
+                    try {
+                        Thread.sleep(300);
+                    } catch (InterruptedException exception) {
+                        System.out.println(exception.getMessage());
+                        return;
+                    }
                 }
             }
+        } catch (IOException exception) {
+            System.out.println(exception.getMessage());
         }
     }
 }
